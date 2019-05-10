48e4c544d8eccd58f404da3da7c3e99559b37ff3
Your Name <you@example.com>
2019-05-10 11:28:14 +0100

    Added usage info.

--- docker-vol-cp.sh
+++ docker-vol-cp.sh
@@ -1,10 +1,23 @@
+#!/bin/bash
 # This command can be used to copy files into a docker volume.
 # The steps it follows are:
 #   1 ) Run a temporary docker volume using the alpine image ( --rm flag )
-#   2 ) Mount the current working directory as /source in the container
+#   2 ) Mount the local directory ( $1 ) as /source in the container
 #   3 ) Mount the destination volume ( $2 ) as /dest in the container
-#   4 ) 
-# note: may not work correctly atm because the cp does not specify the dir
+#   4 ) Use /bin/ash to copy the files across
+
+display_usage ()
+{
+    printf "\n"
+    printf "docker-vol-cp [local_dir] [local_files] [dest_volume] [dest_dir]"
+    printf "\n"
+}
+
+if [ $# -lt 4 ]
+then
+    display_usage
+    exit 1
+fi
 
 local_dir=$(readlink -m -n $1)
 local_files=$2
