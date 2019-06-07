bdfbc077f29bc1c718de979b1f4636454b9ec539
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-07 14:58:08 +0100

    Added docker kill last

---
+++ docker-kill-last.sh
@@ -0,0 +1,13 @@
+#Kills the most recently started docker container.
+
+containerName=docker ps -n 1
+
+printf "Stopping:\n$containerName\n" 
+
+read -r -p "Are you sure? [Y/n]" input
+
+case $input in
+	[yY][eE][sS]|[yY])
+docker stop $( docker ps -aqn 1 )
+;;
+esac
