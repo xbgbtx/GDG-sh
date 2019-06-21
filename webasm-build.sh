5336aaf1deac2d2f0d242d949f59a45fce48d47d
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-21 19:01:10 +0100

    Working command

--- webasm-build.sh
+++ webasm-build.sh
@@ -4,6 +4,12 @@
 #Username@Hostname for the server used to build webassembly
 build_server="boop@web-asm"
 
+build_path="/tmp/wasm-build"
+
+emcc="/home/boop/emsdk/fastcomp/emscripten/emcc"
+
+build_cmd="$emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='[\"cwra\"]'"
+
 display_usage ()
 {
 	printf "\n"
@@ -19,3 +25,16 @@ then
 	display_usage
 	exit 1
 fi
+
+source_path=$(readlink -m -n $1)
+source_file=$(basename $source_path)
+
+ssh $build_server "mkdir -p $build_path"
+
+scp $source_path $build_server:$build_path
+
+ssh $build_server "cd $build_path && $build_cmd $source_file"
+
+scp $build_server:$build_path/a.out.* .
+
+ssh $build_server "rm -rf $build_path"
