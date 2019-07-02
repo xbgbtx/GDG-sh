0a7958e62dc98d5834e3ade7cd401359fe85ec4b
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-07-02 14:49:11 +0100

    Created js-test.sh

--- js-test.sh
+++ js-test.sh
@@ -1,25 +1,25 @@
 # Command to send a source file to the web-assembly build server,
-# compile it and copy the results back
+# and run jest test
 
 #Username@Hostname for the server used to build webassembly
 build_server="boop@web-asm"
 
 #directory to perform the build on the build server
-build_path="/tmp/wasm-build"
+build_path="/tmp/jest-test"
 
-#full path to the emcc binary on the build server
-emcc="/home/boop/emsdk/fastcomp/emscripten/emcc"
+#directory with the testing environment files
+test_env_files="/home/boop/JestTestEnvironment"
 
-#command + otions to perform the build
-build_cmd="$emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='[\"cwrap\"]'"
+#command + otions to perform the tests
+build_cmd="npm run test"
 
 display_usage ()
 {
 	printf "\n"
-	printf "webasm-build.sh [source_file]\n"
+	printf "js-test.sh [source_file]\n"
 	printf "\n"
-	printf "Sends the sourcefile to the web-asm build server, compiles it\n"
-	printf "and copies the a.out files back to the current directory.\n"
+	printf "Sends the sourcefile to the web-asm build server and\n"
+	printf "uses jest to perform unit testing.\n"
 	printf "\n"
 }
 
@@ -36,14 +36,14 @@ source_file=$(basename $source_path)
 #create build dir 
 ssh $build_server "mkdir -p $build_path"
 
+#Copy test environment files
+ssh $build_server "cp -r $test_env_files/* $build_path"
+
 #copy source file to the build dir on the build server
 scp $source_path $build_server:$build_path
 
 #execute the build command
 ssh $build_server "cd $build_path && $build_cmd $source_file"
-
-#copy the output files to current dir on local machine
-scp $build_server:$build_path/a.out.* .
 
 #remove the build folder on build server
 ssh $build_server "rm -rf $build_path"
